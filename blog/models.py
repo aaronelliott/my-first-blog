6e56366f8d25b5a419799c46ffd36a2b0fb6a47a
aaron <aaronelliott321@gmail.com>
2015-06-11 00:14:29 -0400

    changed again

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-#from django.utils import timezone
-import datetime
+from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -12,7 +11,7 @@ class Post(models.Model):
             blank=True, null=True)
 
     def publish(self):
-        self.published_date = datetime.datetime.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
